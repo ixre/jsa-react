ab7a09db93aa0fe1361117d6074ad86ecbcde5dc
jarryliu <jarrysix@gmail.com>
2019-01-09 15:59:20 +0800

    build.sh

--- build.sh
+++ build.sh
@@ -3,7 +3,7 @@
 NODE_ENV=production
 JSA_SRC=../jsa
 
-rm -rf ./dist && rm -rf ${JSA_SRC}/static/app && \
+rm -rf ./dist && rm -rf ${JSA_SRC}/app && \
     parcel build --no-cache --public-url ./ src/index.html && \
-    cp -r dist ${JSA_SRC}/static/app && \
-    find ${JSA_SRC}/static/app -name "*.map"|xargs rm -rf
+    cp -r dist ${JSA_SRC}/app && \
+    find ${JSA_SRC}/app -name "*.map"|xargs rm -rf
