b4df0e46fe6236d6f4419f131f768f28358dcba7
jarryliu <jarrysix@gmail.com>
2019-02-14 17:46:59 +0800

    UserList.js

--- src/components/grid/GridTable.js
+++ src/components/grid/GridTable.js
@@ -1,15 +1,96 @@
 import React from "react";
 import {Table} from "antd";
+import PropsType from "prop-types";
+import {UserIFaceExtend} from "../../lib";
 
-export default class GridTable extends Table {
-    static scrollAuto(table){
-        console.log("---",table.props);
-        return {y:200};
+export default class GridTable extends React.Component {
+    constructor(props) {
+        super(props);
+        this.state = {
+            scrollOffsetY: props.offsetY || 0,
+            scroll: props.scroll,
+            loading: true,
+            pagination: {
+                showQuickJumper: true,
+                pageSize: props.pageSize || 10,
+                current: props.current || 1,
+            },
+        }
+    }
+
+    static propTypes = {
+        offsetY: PropsType.number,
+        ref: PropsType.string,
+        rowKey: PropsType.func,
+        pageSize: PropsType.number,
+        // 获取数据的方法,内部实现如：(params, bind) {bind({rows:[],total:100})
+        dataFetch: PropsType.func,
+        // 数据表格重新加载，值为true时加载
+        reload: PropsType.bool,
+    };
+    static defaultProps = {
+        ref: "table1"
+    };
+    onTableChange = (pagination, filters, sorter) => {
+        const pager = {...this.state.pagination};
+        pager.current = pagination.current;
+        this.setState({
+            pagination: pager,
+        });
+        this.fetchData({
+            size: pagination.pageSize,
+            page: pagination.current,
+            sortField: sorter.field || "",
+            sortOrder: sorter.order || true,
+            ...filters,
+        });
+    }
+
+    fetchData = (params = {
+        size: this.state.pagination.pageSize,
+        page: this.state.pagination.current
+    }) => {
+        this.setState({loading: true});
+        let callback = (data) => {
+            const pagination = {...this.state.pagination};
+            pagination.total = data.total || data.length;
+            this.setState({
+                loading: false,
+                data: data.rows || data.results || data,
+                pagination,
+            });
+        };
+        this.props.dataFetch(params, callback);
+    }
+
+    componentDidMount() {
+        this.fetchData();
+        if (this.state.scroll) {
+            let y = UserIFaceExtend.getTableScrollY(this, this.props.ref);
+            this.setState({scroll: {y: y + this.state.scrollOffsetY}});
+        }
+    }
+
+    componentWillUpdate(nextProps, nextState, nextContext) {
+        if (this.props.reload != nextProps.reload && nextProps.reload == true) {
+            this.fetchData();
+        }
     }
 
     render() {
+        const {onRow, rowSelection, columns, rowKey} = this.props;
         return <div className="gra-grid-table">
-            {super.render()}
+            <Table ref={this.props.ref}
+                   {...this.props}
+                   rowKey={rowKey}
+                   rowSelection={rowSelection}
+                   pagination={this.state.pagination}
+                   dataSource={this.state.data}
+                   columns={columns}
+                   loading={this.state.loading}
+                   scroll={this.state.scroll}
+                   onChange={this.onTableChange}
+                   onRow={onRow ? onRow : null}/>
         </div>;
     }
 }
--- src/features/users/UserList.js
+++ src/features/users/UserList.js
@@ -1,38 +1,60 @@
-import React from "react";
+import React, {Fragment} from "react";
 import GridTable from "../../components/grid/GridTable";
-import Icon from "antd/es/icon";
-import {UserIFaceExtend} from "../../lib";
+import Button from "antd/es/button";
+import {fn, http} from "../../base";
+import Tag from "antd/es/tag";
+import Divider from "antd/es/divider";
+import Search from "antd/es/input/Search";
 
 
 const columns = [{
     title: '用户',
     dataIndex: 'name',
-    width: '40%',
+    width: '30%',
+    render: (name, row) => {
+        return <span>{name}&nbsp;
+            {(row.flag & 2) == 2 ? <Tag color="blue">管理员</Tag> : null}
+        </span>;
+    }
 }, {
     title: '邮箱',
-    dataIndex: 'age',
-    width: '40%'
+    dataIndex: 'email',
+    width: '20%'
 }, {
-    title: '是否启用',
-    dataIndex: 'address'
+    title: '类型',
+    dataIndex: 'flag',
+    width: '20%',
+    render: (flag) => {
+      return flag;
+    }
+}, {
+    title: '操作',
+    dataIndex: '',
+    render: (_, row) => {
+        if ((row.flag & 2) == 2) {
+            return <a href="javascript:;">修改</a>;
+        }
+        return <span>{
+            (row.flag & 1) == 1 ?
+                <a href="javascript:;">停用</a> :
+                <a href="javascript:;">启用</a>
+        }
+            <Divider type="vertical"/>
+            <a href="javascript:;">修改</a>
+        </span>;
+    }
 }];
-
-const data = [];
-for (let i = 0; i < 46; i++) {
-    data.push({
-        key: i,
-        flag: 1,
-        name: `Edward King ${i}`,
-        age: <Icon type="setting"/>,
-        address: `London, Park Lane no. ${i}`,
-    });
-}
 
 
 export class UserList extends React.Component {
     constructor(props) {
         super(props);
-        this.state = {y: 1};
+        this.state = {
+            reload: false,
+            loading: false,
+            searchValue: "",
+            searchHolder: "用户/邮箱"
+        };
         // rowSelection object indicates the need for row selection
         this.rowSelection = {
             onChange: (selectedRowKeys, selectedRows) => {
@@ -45,12 +67,50 @@ export class UserList extends React.Component {
         };
     }
 
-    componentDidMount() {
-        this.setState({y: UserIFaceExtend.getTableScrollY(this, "table1")});
+    dataFetch(params, bind) {
+        // http fetch callback
+        const fc = (() => {
+            return (data) => {
+                bind(data);
+                this.setState({reload: false, loading: false});
+            };
+        }).apply(this);
+        // Attach some params from page
+        params.keyword = this.state.searchValue;
+        // Send fetch request
+        http.jsonPost(fn.api("/user/list"), params, (data) => {
+            fc({total: data.total, rows: data.rows});
+        }, (err) => {
+            fc({total: 0, rows: []});
+        });
+    }
+
+    // Refresh grid table
+    refresh() {
+        this.setState({reload: true, loading: true});
+    }
+
+    onSearch(txt) {
+        this.setState({searchValue: txt,reload:true});
     }
 
     render() {
-        return <GridTable ref="table1" rowSelection={this.rowSelection} scroll={{y: this.state.y}}
-                          columns={columns} dataSource={data}/>
+        const {loading, reload,searchHolder} = this.state;
+        return <React.Fragment>
+            <div className="gra-control-bar">
+                <Button type="primary" onClick={this.refresh}>
+                    新增用户
+                </Button>
+                <span className="gra-divider-x2"/>
+                <Search style={{width: "300px"}} placeholder={searchHolder}
+                        onSearch={this.onSearch.bind(this)}
+                        enterButton/>
+                <span className="gra-divider"/>
+                <Button disabled loading={this.state.loading}>删除</Button>
+            </div>
+            <GridTable rowKey={r=>r.name} bordered reload={reload} dataFetch={this.dataFetch.bind(this)} rowSelection={this.rowSelection}
+                       scroll={{y: 1}}
+                       columns={columns}/>
+        </React.Fragment>
     }
 }
